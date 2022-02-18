c5992fde78b1035d1da46c8feb472b68e7b236e3
UnluckySwede <gadler.michael@elev.ga.ntig.se>
2022-02-18 13:29:29 +0100

    Some javascript

---
+++ app.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const addProfileForm = document.querySelector(".add-profile");
+const switcher = document.querySelector(".btn");
+
+addProfileForm.addEventListener("submit", addProfile);
+
+switcher.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+
+  var className = document.body.className;
+  if (className == "light-theme") {
+    this.textContent = "Dark";
+  } else {
+    this.textContent = "Light";
+  }
+
+  console.log("current class name: " + className);
+});
+
+let profiles = [];
+
+function addProfile(event) {
+  event.preventDefault();
+  let newPerson = {
+    name: "Mikael",
+    birth: 2006,
+    living: true,
+    death: 2020,
+  };
+  profiles.push(newPerson);
+  console.table(profiles);
+}
